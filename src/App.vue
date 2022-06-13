a127bcf4093a9fbe15174aec442824c5b058eee3
Cyprian Nyakundi <cn@Cyprians-MacBook-Pro.local>
2022-06-13 19:54:09 +0300

    Dynamic props

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,22 @@
       <h1>My Friends</h1>
     </header>
     <ul>
-      <friend-contact name= "Manuel Lorenz"
-          phone-number= "0123 45678 90"
-          email-address="manuel@localhost.com" is-favorite="1"></friend-contact>
-      <friend-contact name= "Cyprian Nyakundi"
-          phone-number= "0710280973"
-          email-address="nyakundi@localhost.com" is-favorite="0"></friend-contact>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="true"
+      ></friend-contact>
+
+    
     </ul>
   </section>
 </template>
 
 <script>
 export default {
-
-  
   data() {
     return {
       friends: [
@@ -43,12 +45,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: "Jost", sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -60,11 +65,13 @@ header {
   width: 90%;
   max-width: 40rem;
 }
+
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
+
 #app li {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
@@ -74,12 +81,14 @@ header {
   width: 90%;
   max-width: 40rem;
 }
+
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
+
 #app button {
   font: inherit;
   cursor: pointer;
@@ -89,6 +98,7 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
+
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite  ? "(Favorite)" : "" }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
@@ -25,7 +25,7 @@ export default {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     emailAddress: { type: String, required: true },
-    isFavorite: { type: String, required: false, default: "0" }
+    isFavorite: { type: Boolean, required: false, default: false }
   },
   data() {
     return {
@@ -39,11 +39,10 @@ export default {
     },
 
     toggleFavorite() {
-      if (this.friendIsFavorite === "1") {
-        this.friendIsFavorite = "0";
-      } else {
-        this.friendIsFavorite = "1";
-      }
+
+      this.friendIsFavorite = !this.friendIsFavorite;
+      
+      
     },
   },
 };
